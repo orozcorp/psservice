ab616bf4f595b48d74aa053d360d34834d27cd84
Ed Oz <orozcorp@gmail.com>
2023-10-05 07:15:06 -0600

    updated

--- src/apollo/config/schema.js
+++ src/apollo/config/schema.js
@@ -1,5 +1,7 @@
 import { mergeTypeDefs } from "@graphql-tools/merge";
-import { uploadSchema } from "./schemas/uploadsAndOther.schema";
+import { uploadSchema } from "./types/uploadsAndOthers.schema";
 import { responses } from "./types/responses.schema";
 import { documents } from "./types/Documents.schema";
 const schema = mergeTypeDefs([documents, uploadSchema, responses]);
+
+export default schema;
--- src/app/i18n/client.js
+++ src/app/i18n/client.js
@@ -54,7 +54,7 @@ export function useTranslation(lng, ns, options) {
     useEffect(() => {
       if (cookies.i18next === lng) return;
       setCookie(cookieName, lng, { path: "/" });
-    }, [lng, cookies.i18next]);
+    }, [lng, cookies.i18next, setCookie]);
   }
   return ret;
 }
