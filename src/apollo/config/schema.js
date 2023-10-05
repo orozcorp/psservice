40cecc5aac903905dacaa2e4103e4bec79e44ca5
Ed Oz <orozcorp@gmail.com>
2023-10-05 09:30:07 -0600

    updated

--- src/apollo/config/schema.js
+++ src/apollo/config/schema.js
@@ -1,7 +1,7 @@
 import { mergeTypeDefs } from "@graphql-tools/merge";
 import { uploadSchema } from "./types/uploadsAndOthers.schema";
 import { responses } from "./types/responses.schema";
-import { documents } from "./types/Documents.schema";
+import { documents } from "./types/documents.schema";
 const schema = mergeTypeDefs([documents, uploadSchema, responses]);
 
 export default schema;
