95bf30dd54015ad1435061c4b31e469ee4614d3f
Ed Oz <orozcorp@gmail.com>
2023-10-06 17:03:06 -0600

    testing queries

--- src/app/[lng]/RSE/page.js
+++ src/app/[lng]/RSE/page.js
@@ -5,25 +5,25 @@ import Hero2 from "./Hero2";
 import Respetamos from "./Respetamos";
 import Conocimiento from "./Conocimiento";
 import { getData } from "../../lib/helpers/getData";
-// const QUERY = `
-// query GetDocuments {
-//   getDocuments {
-//     _id
-//     createdAt
-//     estatus
-//     extension
-//     name
-//     url
-//   }
-// }
-// `;
+const QUERY = `
+query GetDocuments {
+  getDocuments {
+    _id
+    createdAt
+    estatus
+    extension
+    name
+    url
+  }
+}
+`;
 export default async function RSE({ params: { lng } }) {
   const { t } = await useTranslation(lng, "RSE");
-  // const query = await getData({
-  //   query: QUERY,
-  // });
-  // const documentos = query?.getDocuments || [];
-  const documentos = [];
+  const query = await getData({
+    query: QUERY,
+  });
+  const documentos = query?.getDocuments || [];
+  // const documentos = [];
   return (
     <div className="w-full mb-96">
       <Hero t={t} />
