fdb16837f2a62d2c8c3ff4e2c4e71304dc7f0f1c
Ed Oz <orozcorp@gmail.com>
2023-10-12 14:34:21 -0600

    updated

--- src/app/[lng]/(components)/Hero.js
+++ src/app/[lng]/(components)/Hero.js
@@ -13,7 +13,7 @@ export default function Hero({ t }) {
       {/* Content */}
 
       <div className="flex flex-col flex-nowrap justify-center items-start w-[90vw] md:w-[75vw] ">
-        <h2 className="text-3xl lg:text-5xl text-[#002772] dark:text-white font-bold py-16">
+        <h2 className="text-3xl lg:text-5xl text-[#002772] dark:text-white font-bold py-16 max-w-2xl">
           {t("Hero1")}
         </h2>
         <button className="py-2 px-4 bg-[#5D001D] text-white rounded-lg">
--- src/app/[lng]/Monitoreo/Hero.js
+++ src/app/[lng]/Monitoreo/Hero.js
@@ -19,7 +19,7 @@ export default function Hero({ t }) {
       {/* Content */}
       <div className="relative z-10 flex flex-row flex-wrap justify-end items-center w-full ">
         <div className="flex flex-col flex-nowrap justify-center items-start w-[90vw] md:w-[75vw] ">
-          <h2 className="text-3xl lg:text-5xl text-[#002772] dark:text-white font-bold py-16">
+          <h2 className="text-3xl lg:text-5xl text-[#002772] dark:text-white font-bold py-16 max-w-2xl">
             {t("Hero1")}
           </h2>
           <button className="py-2 px-4 bg-[#5D001D] text-white rounded-lg">
