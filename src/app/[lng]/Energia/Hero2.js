b9caa64b16c47aea569f33c561c69a201ffbcaa3
Ed Oz <orozcorp@gmail.com>
2023-10-16 16:37:38 -0600

    updated

--- src/app/[lng]/Energia/Hero2.js
+++ src/app/[lng]/Energia/Hero2.js
@@ -8,7 +8,7 @@ export default function Hero2({ t }) {
         <div className="absolute inset-0">
           <HeroBgImage
             lightImg="https://psserviceimages.s3.us-east-2.amazonaws.com/OTHER/SISTEMAS/HERO2_LIGHT.jpg"
-            darkImg="https://psserviceimages.s3.us-east-2.amazonaws.com/dark/SISTEMAS/HERO2.jpgg"
+            darkImg="https://psserviceimages.s3.us-east-2.amazonaws.com/dark/SISTEMAS/HERO2.jpg"
           />
         </div>
       </div>
