e653d483c9cc5f61a5dcc6baf4a2792f4adfc851
Ed Oz <orozcorp@gmail.com>
2023-10-24 15:12:09 -0600

    test

--- src/app/[lng]/(components)/SolucionSingle.js
+++ src/app/[lng]/(components)/SolucionSingle.js
@@ -18,10 +18,10 @@ export default function SolucionSingle({ Icon, title, text, link }) {
         onClick={() => router.push(link)}
       >
         <Icon width="100px" height="100px" />
-        <p className="text-white dark:text-[#FFFFFF] text-sm font-bold">
+        <p className="text-white dark:text-[#FFFFFF] text-lg my-4 font-bold">
           {title}
         </p>
-        <p className="text-white dark:text-[#FFFFFF] text-sm font-bold">
+        <p className="text-stone-300 dark:text-[#FFFFFF] text-sm font-bold">
           {text}
         </p>
       </div>
