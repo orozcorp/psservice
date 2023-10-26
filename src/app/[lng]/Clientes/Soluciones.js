f26002d736d6b542dade2b502233319e13805095
Ed Oz <orozcorp@gmail.com>
2023-10-26 11:03:46 -0600

    updated clientes

--- src/app/[lng]/Clientes/Soluciones.js
+++ src/app/[lng]/Clientes/Soluciones.js
@@ -1,11 +1,24 @@
-export default function Soluciones({ Icon, title, points, colorChg }) {
+import Image from "next/image";
+
+export default function Soluciones({ bgImage, title, points }) {
   return (
     <div
-      className={`flex flex-col flex-nowrap items-center justify-center p-8 min-h-[600px] flex-1 text-white text-center ${colorChg}`}
+      className={`relative flex flex-col flex-nowrap items-center justify-center p-8 min-h-[600px] flex-1 text-white text-center`}
     >
-      <Icon color="#fff" width="120px" height="120px" />
-      <div className="my-8">{title}</div>
-      <div>{points}</div>
+      <Image
+        src={bgImage}
+        alt="Background Image"
+        fill
+        quality={100}
+        className=" object-cover object-center z-0"
+      />
+      <div
+        className="p-4 z-50 font-bold"
+        style={{ backgroundColor: "rgba(39, 39, 42, 0.3)" }}
+      >
+        <div className="my-8">{title}</div>
+        <div>{points}</div>
+      </div>
     </div>
   );
 }
--- src/app/[lng]/Clientes/page.js
+++ src/app/[lng]/Clientes/page.js
@@ -2,36 +2,37 @@ import { useTranslation } from "../../i18n";
 import Hero from "./Hero";
 import Somos from "./Somos";
 import Soluciones from "./Soluciones";
-import SolucionesPoints from "./SolucionesPoints";
-import Icon1 from "../../components/SVG/TecnologiaMed/Icon1";
-import Icon2 from "../../components/SVG/TecnologiaMed/Icon2";
-import Icon3 from "../../components/SVG/TecnologiaMed/Icon3";
-import Image from "next/image";
-import Contacta from "./Contacta";
+
 import Contacto from "../(components)/Contacto";
 export default async function Fabricacion({ params: { lng } }) {
   const { t } = await useTranslation(lng, "Clientes");
   const solucionesData = [
     {
       id: 0,
-      Icon: Icon1,
+
       title: t("Soluciones1"),
       points: t("Soluciones1Points"),
       colorChg: "bg-[#2E7D96]",
+      bgImage:
+        "https://psserviceimages.s3.us-east-2.amazonaws.com/OTHER/CLIENTES/CLIENTE_01.jpg",
     },
     {
       id: 1,
-      Icon: Icon2,
+
       title: t("Soluciones2"),
       points: t("Soluciones2Points"),
       colorChg: "bg-[#155E75]",
+      bgImage:
+        "https://psserviceimages.s3.us-east-2.amazonaws.com/OTHER/CLIENTES/CLIENTE_02.jpg",
     },
     {
       id: 2,
-      Icon: Icon3,
+
       title: t("Soluciones3"),
       points: t("Soluciones3Points"),
       colorChg: "bg-[#044053]",
+      bgImage:
+        "https://psserviceimages.s3.us-east-2.amazonaws.com/OTHER/CLIENTES/CLIENTE_03.jpg",
     },
   ];
   return (
@@ -39,7 +40,7 @@ export default async function Fabricacion({ params: { lng } }) {
       <Hero t={t} />
       <Somos t={t} />
       <div className="flex flex-col lg:flex-row flex-nowrap lg:flex-wrap w-full mb-16 items-center justify-center">
-        <SolucionesPoints {...solucionesData[0]} />
+        <Soluciones {...solucionesData[0]} />
         <Soluciones {...solucionesData[1]} />
         <Soluciones {...solucionesData[2]} />
       </div>
