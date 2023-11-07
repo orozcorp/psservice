b4dad5662e6943181988552a5610db0681c1a0d5
Ed Oz <orozcorp@gmail.com>
2023-11-07 13:06:26 -0600

    updated

--- src/app/[lng]/Dashboard/DocumentUpload/page.js
+++ src/app/[lng]/Dashboard/DocumentUpload/page.js
@@ -10,14 +10,14 @@ export default function DocumentUpload() {
       </h1>
       <div className="flex flex-wrap flex-row justify-around w-full items-center gap-8">
         <Link
-          href="/DocumentUpload/UploadDocument"
+          href="/Dashboard/DocumentUpload/UploadDocument"
           className="text-[#0A2473] bg-[#EBEAEA] hover:bg-[#9C9A9A] focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-lg px-5 py-2.5 mr-2 mb-2"
         >
           Subir un documento
           <AiOutlineCloudUpload className="inline-block ml-6 w-6 h-6" />
         </Link>
         <Link
-          href="/DocumentUpload/PublishedDocuments"
+          href="/Dashboard/DocumentUpload/PublishedDocuments"
           className="text-[#0A2473] bg-[#EBEAEA] hover:bg-[#9C9A9A]  focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-lg px-5 py-2.5 mr-2 mb-2"
         >
           Documentos Publicados
