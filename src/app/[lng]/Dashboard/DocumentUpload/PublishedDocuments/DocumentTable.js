3f14d676b84938c11ba85cfacd9975136d07602e
Ed Oz <orozcorp@gmail.com>
2023-11-09 06:03:25 -0600

    updated

--- src/app/[lng]/Dashboard/DocumentUpload/PublishedDocuments/DocumentTable.js
+++ src/app/[lng]/Dashboard/DocumentUpload/PublishedDocuments/DocumentTable.js
@@ -70,7 +70,9 @@ export default function DocumentTable({ initialDocuments }) {
         <table className="w-fit text-sm text-left text-gray-500 ">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
             <tr>
-              <th scope="col" className="px-6 py-3"></th>
+              <th scope="col" className="px-6 py-3">
+                Selecciona para borrar
+              </th>
               <th scope="col" className="px-6 py-3">
                 <div
                   className="flex items-center"
