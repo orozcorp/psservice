0b0fb11ed213032b661d6673b71965a9f8ac5c0b
Ed Oz <orozcorp@gmail.com>
2023-11-22 14:44:10 -0600

    test

--- src/app/components/Navbar/NavbarBase.js
+++ src/app/components/Navbar/NavbarBase.js
@@ -52,6 +52,8 @@ export const NavbarBase = ({ t, lng, i18n }) => {
               </div>
             </div>
           </div>
+
+
           {width > 1000 && (
             <div className="flex flex-row flex-wrap justify-end gap-8 items-start content-center text-white mr-4 w-full">
               <Link
