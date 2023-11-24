8d00bba485278524fd5d3f4fc8dd468100e98eb1
Ed Oz <orozcorp@gmail.com>
2023-11-24 05:50:57 -0600

    updated the meta tags

--- src/app/[lng]/EmpresaSocialmenteResponsable/layout.js
+++ src/app/[lng]/EmpresaSocialmenteResponsable/layout.js
@@ -1,8 +1,18 @@
-export const metadata = {
-  title: "Empresa Socialmente Responsable PS Services",
-  description:
-    "Comprometidos con la responsabilidad social empresarial en México, impulsamos prácticas sostenibles y contribuimos al desarrollo de nuestras comunidades",
-};
+export async function generateMetadata({ params }) {
+  const lng = params.lng;
+  if (lng === "en") {
+    return {
+      title: "Socially Responsible Company",
+      description:
+        "Committed to corporate social responsibility in Mexico, we promote sustainable practices and contribute to the development of our communities",
+    };
+  }
+  return {
+    title: "Empresa Socialmente Responsable PS Services",
+    description:
+      "Comprometidos con la responsabilidad social empresarial en México, impulsamos prácticas sostenibles y contribuimos al desarrollo de nuestras comunidades",
+  };
+}
 
 export default async function Layout({ children }) {
   return <>{children}</>;
--- src/app/[lng]/ImplementacionDeTecnologiaMedica/layout.js
+++ src/app/[lng]/ImplementacionDeTecnologiaMedica/layout.js
@@ -4,6 +4,22 @@ export const metadata = {
     "Innovamos en la instalación de tecnología médica en México, ofreciendo soluciones avanzadas para el sector de la salud. Comprometidos con la calidad y la eficacia",
 };
 
+export async function generateMetadata({ params }) {
+  const lng = params.lng;
+  if (lng === "en") {
+    return {
+      title: "Medical Technology Implementation PS Services",
+      description:
+        "We innovate in the installation of medical technology in Mexico, offering advanced solutions for the health sector. Committed to quality and efficiency",
+    };
+  }
+  return {
+    title: "Implementación de Tecnología Médica PS Services",
+    description:
+      "Innovamos en la instalación de tecnología médica en México, ofreciendo soluciones avanzadas para el sector de la salud. Comprometidos con la calidad y la eficacia",
+  };
+}
+
 export default async function Layout({ children }) {
   return <>{children}</>;
 }
