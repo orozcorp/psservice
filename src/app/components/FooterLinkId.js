c4a0c6f76f6d9d0b76c352c3c3574bfc5ca997ca
Ed Oz <orozcorp@gmail.com>
2023-11-30 18:08:37 -0600

    updated

--- src/app/components/FooterLinkId.js
+++ src/app/components/FooterLinkId.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function FooterLinksId({ lng, href, text }) {
   return (
-    <li className="my-2">
+    <li className="my-3 lg:my-2">
       <Link href={`/${lng}/#${href}`}>{text}</Link>
     </li>
   );
