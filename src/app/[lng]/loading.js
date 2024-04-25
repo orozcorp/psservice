287585ed1e546123c9ad1dcf13a26ad80db9ef43
Orozcorp <orozcorp@gmail.com>
2024-04-25 15:47:50 -0600

    Updating to Next 14.2.3

--- src/app/[lng]/loading.js
+++ src/app/[lng]/loading.js
@@ -1,7 +1,5 @@
 export default function Loading() {
   return (
-    <div className="w-screen h-[50vh] border flex flex-col flex-nowrap">
-      loading
-    </div>
+    <div className="w-screen h-[50vh] border flex flex-col flex-nowrap animate-pulse bg-slate-400"></div>
   );
 }
